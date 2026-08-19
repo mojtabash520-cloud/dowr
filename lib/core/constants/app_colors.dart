aaaa9767d724eac5637ab035dc1a798f8127bba4
mojtabash520-cloud <mojtaba.sh520@gmail.com>
2026-08-19 19:06:16 +0330

    Update app_colors.dart

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,5 +1,16 @@
-// Define your colors here
 import 'package:flutter/material.dart';
+
 class AppColors {
-  static const primary = Colors.indigo;
+  // رنگ‌های اصلی تم
+  static const Color primary = Color(0xFF6C63FF);
+  static const Color background = Color(0xFFF4F6F9);
+  
+  // رنگ‌های دکمه‌ها و اکشن‌ها
+  static const Color success = Color(0xFF00C853);
+  static const Color danger = Color(0xFFFF6584);
+  static const Color warning = Color(0xFFFFC045);
+  
+  // رنگ‌های متن
+  static const Color textMain = Colors.black87;
+  static const Color textMuted = Colors.grey;
 }
